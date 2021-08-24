e9c899a913fed354b00367703484e3d6c3a5c703
Istvan <steven.ford.j@gmail.com>
2021-08-24 15:13:01 +0300

    update

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -4,7 +4,7 @@ import {Component} from 'react'
 
 class Home extends Component {
     state = {
-        selectedDish: dishes[0]
+        selectedDish: null
     }
 
     render (){
@@ -13,33 +13,33 @@ class Home extends Component {
             <Container>
                 <Row className="mt-3 justify-content-center">
                     <Col xs={12} md={10}>
-            <Carousel>
-                {
-                    dishes.map(dish => (
-                        <Carousel.Item key={dish.id}>
-                        <img
-                            className="d-block w-100"
-                            src={dish.image}
-                            alt={dish.name}
-                            onClick={() => this.setState({
-                                selectedDish: dish
-                            })}
-                        />
-                        <Carousel.Caption>
-                            <h3>{dish.name}</h3>
-                            <p>{dish.description}</p>
-                        </Carousel.Caption>
-                        </Carousel.Item>
-                    ))
-                }
-            </Carousel>
+                        <Carousel>
+                            {
+                                dishes.map(dish => (
+                                    <Carousel.Item key={dish.id}>
+                                    <img
+                                        className="d-block w-100"
+                                        src={dish.image}
+                                        alt={dish.name}
+                                        onClick={() => this.setState({
+                                            selectedDish: dish
+                                        })}
+                                    />
+                                    <Carousel.Caption>
+                                        <h3>{dish.name}</h3>
+                                        <p>{dish.description}</p>
+                                    </Carousel.Caption>
+                                    </Carousel.Item>
+                                ))
+                            }
+                        </Carousel>
                     </Col>
                 </Row>
                 <Row>
                     <ul>
                         {
-                            this.state.selectedDish.comments.map(c =>(
-                                <li>{c.comment}</li>
+                            this.state.selectedDish?.comments.map(c =>(
+                                <li key={c.id}>{c.comment}</li>
                             ))
                         }
                     </ul>
